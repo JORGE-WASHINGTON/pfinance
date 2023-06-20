566634acb734d4020393e0221d8da969576be034
jorge <jwoliveira18@gmail.com>
2023-06-21 00:12:45 +1000

    service parameter is now the CreateUserRequestDto, DTO to mapping happens on the service

--- src/main/java/com/pfinance/demo/Services/UserService.java
+++ src/main/java/com/pfinance/demo/Services/UserService.java
@@ -1,6 +1,8 @@
 package com.pfinance.demo.Services;
 
+import com.pfinance.demo.DTO.CreateUserRequestDTO;
 import com.pfinance.demo.Entities.User;
+import com.pfinance.demo.Mappers.UserMapper;
 import com.pfinance.demo.Repositories.UserRepository;
 import org.springframework.stereotype.Service;
 
@@ -10,13 +12,15 @@ import java.util.List;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, UserMapper userMapper) {
         this.userRepository = userRepository;
+        this.userMapper = userMapper;
     }
 
-    public User createUser(User newUser){
-        return userRepository.save(newUser);
+    public User createUser(CreateUserRequestDTO createUserRequestDTO){
+        return userRepository.save(userMapper.UserDtotoUser(createUserRequestDTO));
     }
 
     public List<User> getUsers(){
