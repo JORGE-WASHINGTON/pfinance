777875c032f8d3e34de90b5503891af2b780c6bb
jorge <jwoliveira18@gmail.com>
2023-06-20 12:16:49 +1000

    added simple tests for saving and retrieving transactions resources.

--- src/test/java/com/pfinance/demo/Repositories/TransactionRepositoryTest.java
+++ src/test/java/com/pfinance/demo/Repositories/TransactionRepositoryTest.java
@@ -9,6 +9,9 @@ import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
@@ -20,7 +23,7 @@ class TransactionRepositoryTest {
     private AccountRepository accountRepository;
 
     @Test
-    void shouldCreateTransaction() {
+    void shouldCreateTransactionAndFieldsMatch() {
         //given
         Account newAccount = new Account(new BigDecimal(10), "anz");
         Account savedAccount = accountRepository.save(newAccount);
@@ -31,7 +34,28 @@ class TransactionRepositoryTest {
 
         //then
         assertNotNull(savedTransaction.getId());
-        assertEquals(savedTransaction.getAccount().getId(), savedAccount.getId());
+        assertEquals(newTransaction.getAccount(), savedTransaction.getAccount());
+        assertEquals(newTransaction.getAmount(), savedTransaction.getAmount());
+        assertEquals(newTransaction.getDescription(), savedTransaction.getDescription());
+        assertEquals(newTransaction.getType(), savedTransaction.getType());
+        assertEquals(newTransaction.getCategory(), savedTransaction.getCategory());
+        assertEquals(newTransaction.getDate(), savedTransaction.getDate());
+    }
+
+    @Test
+    void shouldRetrieveAllTransactions() {
+        //given
+        Account newAccount = new Account(new BigDecimal(10), "anz");
+        Account savedAccount = accountRepository.save(newAccount);
+        Transaction transaction1 = new Transaction(savedAccount, new BigDecimal(5), "myki", Transaction.TransactionType.EXPENSE, Transaction.Category.TRANSPORTATION, LocalDate.of(2023, 6, 30), "myki top up");
+        Transaction transaction2 = new Transaction(savedAccount, new BigDecimal(5), "myki", Transaction.TransactionType.EXPENSE, Transaction.Category.TRANSPORTATION, LocalDate.of(2023, 6, 30), "myki top up");
+        Transaction transaction3 = new Transaction(savedAccount, new BigDecimal(5), "myki", Transaction.TransactionType.EXPENSE, Transaction.Category.TRANSPORTATION, LocalDate.of(2023, 6, 30), "myki top up");
+        List<Transaction> transactions = Arrays.asList(transaction1, transaction2, transaction3);
+        transactionRepository.saveAll(transactions);
+        //when
+        List<Transaction> retrievedTransactions = transactionRepository.findAll();
+        //then
+        assertEquals(retrievedTransactions.size(), transactions.size());
     }
 
 }
