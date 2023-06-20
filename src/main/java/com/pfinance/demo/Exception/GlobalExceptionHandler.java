4f9da532f1fa122ac0c0df1af3d87d6fdb0f2c10
jorge <jwoliveira18@gmail.com>
2023-06-21 00:10:57 +1000

    Added class for exception handling

---
+++ src/main/java/com/pfinance/demo/Exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.pfinance.demo.Exception;
+
+import org.hibernate.PropertyValueException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(PropertyValueException.class)
+    public ResponseEntity<Object> handlePropertyValueException(PropertyValueException ex) {
+        String errorMessage = ex.getMessage();
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(errorMessage);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        String errorMessage = ex.getMessage();
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(errorMessage);
+    }
+}
