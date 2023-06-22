c3b486a5bbc62f92c0842837aa5ad1c6a90478ff
jorge <jwoliveira18@gmail.com>
2023-06-22 20:15:00 +1000

    create account method exception handling

--- src/main/java/com/pfinance/demo/Services/AccountService.java
+++ src/main/java/com/pfinance/demo/Services/AccountService.java
@@ -1,6 +1,9 @@
 package com.pfinance.demo.Services;
 
+import com.pfinance.demo.DTO.CreateAccountRequestDTO;
 import com.pfinance.demo.Entities.Account;
+import com.pfinance.demo.Entities.User;
+import com.pfinance.demo.Mappers.AccountMapper;
 import com.pfinance.demo.Repositories.AccountRepository;
 import org.springframework.stereotype.Service;
 
@@ -8,16 +11,25 @@ import org.springframework.stereotype.Service;
 public class AccountService {
 
     private final AccountRepository accountRepository;
+    private final UserService userService;
+    private final AccountMapper accountMapper;
 
-    protected AccountService(AccountRepository accountRepository) {
+    protected AccountService(AccountRepository accountRepository, UserService userService, AccountMapper accountMapper) {
         this.accountRepository = accountRepository;
+        this.userService = userService;
+        this.accountMapper = accountMapper;
     }
 
-    public Account createAccount(Account newAccount) {
-        return accountRepository.save(newAccount);
-    }
+        public Account createAccount(Long userId, CreateAccountRequestDTO createAccountRequestDTO) {
+            User user = userService.getUserById(userId);
+            Account newAccount = accountMapper.AccountDtotoAccount(createAccountRequestDTO);
+            newAccount.setUser(user);
+            try {
+                return accountRepository.save(newAccount);
+            } catch (Exception ex){
+                System.out.println("An error occurred while creating the account: " + ex.getMessage());
+                throw ex;
+            }
+        }
 
-    public void deleteAccount(Integer id) {
-        accountRepository.deleteById(id);
-    }
 }
