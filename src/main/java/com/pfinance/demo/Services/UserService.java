f00b4867cbd3b0d59403df7b072f488084e6c904
jorge <jwoliveira18@gmail.com>
2023-06-22 20:17:27 +1000

    defined getUserByID method with exception handling

--- src/main/java/com/pfinance/demo/Services/UserService.java
+++ src/main/java/com/pfinance/demo/Services/UserService.java
@@ -4,6 +4,7 @@ import com.pfinance.demo.DTO.CreateUserRequestDTO;
 import com.pfinance.demo.Entities.User;
 import com.pfinance.demo.Mappers.UserMapper;
 import com.pfinance.demo.Repositories.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,11 +21,28 @@ public class UserService {
     }
 
     public User createUser(CreateUserRequestDTO createUserRequestDTO){
-        return userRepository.save(userMapper.UserDtotoUser(createUserRequestDTO));
+        try {
+            return userRepository.save(userMapper.UserDtotoUser(createUserRequestDTO));
+        } catch (Exception ex) {
+            throw ex;
+        }
+
     }
 
     public List<User> getUsers(){
         return userRepository.findAll();
+    }
+
+    public User getUserById(Long id) {
+        try {
+            return userRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("User not found"));
+        } catch (EntityNotFoundException ex) {
+            System.out.println(ex.getMessage());
+            throw ex;
+        } catch (Exception ex) {
+            System.out.println("An error occurred while retrieving the user: " + ex.getMessage());
+        }
+        return null;
     }
 
 }
